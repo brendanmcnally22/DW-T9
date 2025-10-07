ffedf0ab844ab279c1c8bc65ddc38c5f3a8b7ab0
brendanmcnally22 <brendanmcnally295@gmail.com>
2025-10-07 18:37:05 -0400

    Add Code - Add Timer.cs - Add UI.cs - Add RunProgram.cs - Add Inventory.cs

---
+++ DW-T9/Game/RunProgram.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DW_T9.Game
+{
+    public static class RunProgram
+    {
+        public static void Main(string[] args)
+        {
+            try
+            {
+                var game = new Game();
+                game.Run();
+            }
+            catch (Exception ex)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Fatal error: " + ex.Message);
+                Console.ResetColor();
+            }
+        }
+    }
+}
